e6bc311b7a63092dcc7d72ddb427c07d81746953
Wahid Ahmed Chowdhury <wahidchowdhury9000@gmail.com>
2023-06-07 02:22:57 +0600

    basic setup done

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-2xl">This is Home</h2>
+            
+            
+        </div>
+    );
+};
+
+export default Home;
