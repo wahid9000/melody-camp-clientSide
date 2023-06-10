c17feaa6c82d46e5844f975d3c9ebd913f4eca73
Wahid Ahmed Chowdhury <wahidchowdhury9000@gmail.com>
2023-06-11 02:28:43 +0600

    payment history data fetched using axios secure

--- src/Pages/Dashboard/StudentDashboard/PaymentHistory.jsx
+++ src/Pages/Dashboard/StudentDashboard/PaymentHistory.jsx
@@ -1,10 +1,24 @@
 
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useState } from "react";
+
 const PaymentHistory = () => {
+    const [axiosSecure] = useAxiosSecure();
+    const [paymentHistory, setPaymentHistory] = useState([]);
+
+    axiosSecure.get('/paymentHistory')
+    .then(res => {
+        console.log(res.data);
+        setPaymentHistory(res.data)
+    })
+
     return (
         <div>
-            <h2 className="text-3xl font-semibold">My Payment History</h2>
+            <h2 className="text-3xl font-semibold">My Payment History: {paymentHistory.length}</h2>
 
-            
+            <div>
+
+            </div>
         </div>
     );
 };
