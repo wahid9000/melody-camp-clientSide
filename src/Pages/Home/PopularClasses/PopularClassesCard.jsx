54658876b1e19c4f0d45fc0a57d370b6c5f4bae3
Wahid Ahmed Chowdhury <wahidchowdhury9000@gmail.com>
2023-06-18 00:40:57 +0600

    code updated

--- src/Pages/Home/PopularClasses/PopularClassesCard.jsx
+++ src/Pages/Home/PopularClasses/PopularClassesCard.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { FaArrowRight } from 'react-icons/fa';
 
 const PopularClassesCard = ({ singleClass }) => {
-    const { class_name, class_image, enrolledStudents } = singleClass;
+    const { class_name, class_image, enrolledStudents, price } = singleClass;
     // 
     return (
         <div className="card  flex-col pb-5 border-y-4 rounded-none w-full mx-auto">
@@ -13,10 +13,11 @@ const PopularClassesCard = ({ singleClass }) => {
                 </figure>
             </div>
             <div className="mt-8 px-5">
-                <h2 className="text-2xl font-semibold mb-4 font-primary">{class_name}</h2>
+                <h2 className="text-2xl font-semibold font-primary">{class_name}</h2>
+                <p className="mb-5 text-lg font-secondary  ">${price}</p>
                 <Link to='/classes'><button
-                    className="btn font-secondary capitalize rounded-md bg-white border-b-4 border-blue-950 px-2 text-sm font-medium hover:border-black hover:bg-blue-950 text-blue-950 hover:bg-opacity-90 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-                > Enroll Now <FaArrowRight></FaArrowRight> 
+                    className="btn font-secondary font-semibold capitalize rounded-md bg-white border-b-4 border-blue-950 px-2 text-sm  hover:border-black hover:bg-blue-950 text-blue-950 hover:bg-opacity-90 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+                > See Details <FaArrowRight></FaArrowRight> 
                     </button></Link>
             </div>
         </div>
